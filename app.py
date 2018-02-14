249d776bcfdaafb63dd8b500a19bea64ba50f39f
Cam <personaontherun@gmail.com>
2018-02-14 13:22:16 -0500

    0.2.5 first patch

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import random
 
 
-#Version 0.2.2
+#Version 0.2.5
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top-secret!'
@@ -24,74 +24,6 @@ celery.conf.update(app.config)
 def search(txtime, pricedata):
     return [element for element in pricedata if datetime.datetime.fromtimestamp(element['time']).strftime('%x') == txtime]
 
-# def apicall1(zenaddress):
-#     s = requests.Session()
-#     link1 = 'https://explorer.zensystem.io/insight-api-zen/addr/'+zenaddress
-#     req = requests.Request('GET', link1)
-#     r = req.prepare()
-#     link1data = s.send(r)
-#     link1data = link1data.json()
-#     txlist = link1data['transactions']
-#     # we were figuring out a way to increment the count1 and count2 on each loop. results caps at 1000
-#     # thicc
-#     while len(txlist) % 1000 == 0:
-#         count1 = len(txlist) + 1
-#         count2 = count1 + 1000
-#         txlink = 'https://explorer.zensystem.io/insight-api-zen/addr/'+str(zenaddress)+'?from='+str(count1)+'&to='+str(count2)
-#         req = requests.Request('GET', txlink)
-#         r = req.prepare()
-#         txdata = s.send(r)
-#         txdata = txdata.json()
-#         txresult = txdata['transactions']
-#         txlist = txlist + txresult
-#     return txlist
-
-
-# def apicall2(tx,zenaddress,pricedata):
-#     s = requests.Session()
-#     link2 = 'https://explorer.zensystem.io/insight-api-zen/tx/'
-#     print(tx)
-#     row = []
-#     templink = link2+tx
-#     print(templink)
-#     req = requests.Request('GET', templink)
-#     r = req.prepare()
-#     tempdata = s.send(r)
-#     tempdata = tempdata.json()
-#     txtime = datetime.datetime.fromtimestamp(tempdata['time']).strftime('%x')
-#     print(txtime)
-#     row.append(datetime.datetime.fromtimestamp(tempdata['time']).strftime('%Y-%m-%d'))
-#     for n in tempdata['vout']:
-#         if n['scriptPubKey']['addresses'][0] == zenaddress:
-#             row.append(n['value'])
-#     closeprice = search(txtime, pricedata)
-#     try:
-#         closeprice = closeprice[0]['close']
-#     except IndexError:
-#         closeprice = 0
-#     row.append(closeprice)
-#     value = float(closeprice)*float(row[1])
-#     row.append(value)
-#     try:
-#         if tempdata['vin'][0]['addr'] != zenaddress:
-#             row.append(tempdata['vin'][0]['addr'])
-#             return row
-#     except IndexError:
-#         row.append('zk transaction')
-#         return row
-
-# def apicall3():
-#     s = requests.Session()
-#     pricelink = 'https://min-api.cryptocompare.com/data/histoday?fsym=ZEN&tsym=USD&allData=true&aggregate=1&e=CCCAGG'
-#     preq = requests.Request('GET', pricelink)
-#     pr = preq.prepare()
-#     pricedata = s.send(pr)
-#     pricedata = pricedata.json()
-#     pricedata = pricedata['Data']
-#     print('WHY AINT I WORKING')
-#     print(pricedata)
-#     return pricedata
-
 @celery.task(bind=True)
 def zentask(self,zenaddress):
     print(zenaddress)
@@ -107,6 +39,7 @@ def zentask(self,zenaddress):
     pr = preq.prepare()
     pricedata = s.send(pr)
     pricedata = pricedata.json()
+    # get the price data
     pricedata = pricedata['Data']
 
     s = requests.Session()
@@ -116,7 +49,7 @@ def zentask(self,zenaddress):
     data = s.send(r)
     data = data.json()
     pages = data['pagesTotal']
-    rowlist = [["Time", "Amount", "Price","Value","Sender"]]
+    rowlist = [["Time", "Amount", "Price","Value","Tx Address","Sender"]]
 
     total = pages
     print("total number of pages: ",pages)
@@ -131,7 +64,6 @@ def zentask(self,zenaddress):
         self.update_state(state='PROGRESS',
                           meta={'current': i, 'total': total,
                                 'status': message})
-
         page = n
         print(page)
         if total == 1:
@@ -163,6 +95,7 @@ def zentask(self,zenaddress):
             print("appended closeprice")
             value = float(closeprice)*float(row[1])
             row.append(value)
+            row.append(tx['txid'])
             print("appended value")
             try:
                 if tx['vin'][0]['addr'] != zenaddress:
@@ -176,56 +109,6 @@ def zentask(self,zenaddress):
     self.update_state(state='COMPLETE')
     print(rowlist)
     return {'current': 100, 'total': 100, 'status': 'Task completed!', 'result': rowlist}
-    # s = requests.Session()
-    # link1 = 'https://explorer.zensystem.io/insight-api-zen/addr/'+zenaddress
-    #
-    # link2 = 'https://explorer.zensystem.io/insight-api-zen/tx/'
-    # pricelink = 'https://min-api.cryptocompare.com/data/histoday?fsym=ZEN&tsym=USD&allData=true&aggregate=1&e=CCCAGG'
-    #
-    # req = requests.Request('GET', link1)
-    # r = req.prepare()
-    # link1data = s.send(r)
-    # link1data = link1data.json()
-    # preq = requests.Request('GET', pricelink)
-    # pr = preq.prepare()
-    # pricedata = s.send(pr)
-    # pricedata = pricedata.json()
-    # for tx in link1data['transactions']:
-    #     #silly stuff
-    #     i = i+1
-    #     if not message or random.random() < 0.25:
-    #         message = '{0} {1} {2}...'.format(random.choice(verb),
-    #                                           random.choice(adjective),
-    #                                           random.choice(noun))
-    #     self.update_state(state='PROGRESS',
-    #                       meta={'current': i, 'total': total,
-    #                             'status': message})
-    #     print(tx)
-    #     row = []
-    #     templink = link2+tx
-    #     req = requests.Request('GET', templink)
-    #     r = req.prepare()
-    #     tempdata = s.send(r)
-    #     tempdata = tempdata.json()
-    #     txtime = datetime.datetime.fromtimestamp(tempdata['time']).strftime('%x')
-    #     row.append(datetime.datetime.fromtimestamp(tempdata['time']).strftime('%Y-%m-%d'))
-    #     for n in tempdata['vout']:
-    #         if n['scriptPubKey']['addresses'][0] == zenaddress:
-    #             row.append(n['value'])
-    #     closeprice = search(txtime, pricedata)
-    #     closeprice = closeprice[0]['close']
-    #     row.append(closeprice)
-    #     value = float(closeprice)*float(row[1])
-    #     row.append(value)
-    #     try:
-    #         if tempdata['vin'][0]['addr'] != zenaddress:
-    #             row.append(tempdata['vin'][0]['addr'])
-    #             rowlist.append(row)
-    #     except IndexError:
-    #         row.append('zk transaction')
-    #         rowlist.append(row)
-
-
 
 @app.route('/',methods = ['GET'])
 def home():
--- zapp.sh
+++ zapp.sh
@@ -1,3 +1,3 @@
 $HOME/redis/src/redis-server &&
-celery worker -A $HOME/dropbox/projects/flask-easy-zen/app.celery --loglevel=info && 
+celery worker --app=app.celery --loglevel=info &&
 python3 $HOME/dropbox/projects/flask-easy-zen/app.py
